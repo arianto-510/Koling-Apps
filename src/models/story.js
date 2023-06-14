da4c4ada668623341cc60b25968188f0c00b7c25
arianto-510 <97211033@student.itk.ac.id>
2023-06-14 14:54:32 +0800

    add create story

--- src/models/story.js
+++ src/models/story.js
@@ -9,13 +9,8 @@ const getAllStory = () => {
 
 const addNewStory = (body) => {
   const idUser = `story${nanoid(15)}`;
-  const date = new Date();
-  console.log(date);
-  const query = `INSERT INTO story (id, name, description, photoUrl, createAt, lan, lon) VALUES ('${idUser}','${body.name}', '${body.description}', '${body.photoUrl}', '${date}','${body.lan}', '${body.lon}')`;
+  const query = `INSERT INTO story (id, name, description, photoUrl, lan, lon) VALUES ('${idUser}','${body.name}', '${body.description}', '${body.photoUrl}','${body.lan}', '${body.lon}')`;
   return dbPool.execute(query);
 };
 
-// const updateStory = (body, idUser) => {
-//   const query = `UPDATE story SET `;
-// };
 module.exports = { getAllStory, addNewStory };
